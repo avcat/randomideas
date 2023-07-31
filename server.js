6d3c065a5c767f0a86f012c97e60279079e451b7
arkadii.vodolazskyi <arkadii.vodolazskyi@gmail.com>
2023-07-31 19:04:47 +0300

    Installed Nodemon - automatically restarts server on changes. Added route for ideas. Code clean up.

--- server.js
+++ server.js
@@ -6,56 +6,10 @@ const paths = {
 };
 const app = express();
 
-const ideas = [
-  {
-    id: 1,
-    text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
-    tag: 'Technology',
-    username: 'TonyStark',
-    date: '2022-01-02',
-  },
-  {
-    id: 2,
-    text: 'Milk cartons that turn a different color the older that your milk is getting',
-    tag: 'Inventions',
-    username: 'SteveRogers',
-    date: '2022-01-02',
-  },
-  {
-    id: 3,
-    text: 'ATM location app which lets you know where the closest ATM is and if it is in service',
-    tag: 'Software',
-    username: 'BruceBanner',
-    date: '2022-01-02',
-  },
-];
+// Establishing basic routes
 
 app.get('/', (req, res) => {
   res.send('RandomIdeas API');
-});
-
-app.get(`/${paths.ideas}`, (req, res) => {
-  res.json({
-    success: true,
-    data: ideas,
-  });
-});
-
-app.get(`/${paths.ideas}/:id`, (req, res) => {
-  const ideaID = Number(req.params.id);
-  const idea = ideas.find((idea) => idea.id === ideaID);
-
-  if (!idea) {
-    res.status(404).json({
-      success: false,
-      error: `Idea with ID ${ideaID} was not found.`,
-    });
-  }
-
-  res.json({
-    success: true,
-    data: idea,
-  });
 });
 
 app.listen(port, () => {
@@ -65,3 +19,8 @@ app.listen(port, () => {
     `Find a specific ID, f.e.: http://localhost:${port}/${paths.ideas}/1`
   );
 });
+
+// Advanced routes
+
+import ideasRouter from './routes/ideas.js';
+app.use(`/${paths.ideas}`, ideasRouter);
